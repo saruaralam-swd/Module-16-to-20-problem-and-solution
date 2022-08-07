11f14a1f646e205652a6b156f50c0a95f665b7f7
saruaralam-swd <saruaralam2001@gmail.com>
2022-08-07 17:21:51 +0600

    Module-16-to-20-problem-and-solution-skfjd

---
+++ m17-solutions.js
@@ -0,0 +1,197 @@
+// Note TODO:
+// =========================//=========================//=========================
+// প্রাকটিস চ্যালেঞ্জ-1: 
+// =========================//=========================//=========================
+
+// Using IF/ELSE
+// var marks = 100;
+// if (marks >= 80 && marks <= 100) {
+//     console.log('You Got A+');
+// }
+// else if (marks >= 70 && marks <= 79) {
+//     console.log('You Got A');
+// }
+// else if (marks >= 60 && marks <= 69) {
+//     console.log('You Got B+');
+// }
+// else if (marks >= 50 && marks <= 59) {
+//     console.log('You Got B');
+// }
+// else if (marks >= 40 && marks <= 49) {
+//     console.log('You Got C');
+// }
+// else if (marks >= 33 && marks <= 39) {
+//     console.log('You Got D');
+// }
+// else if (marks >= 0 && marks <= 32) {
+//     console.log('You Failed');
+// }
+// else {
+//     console.log('Please Input a Valid Number from 0 to 100');
+// }
+
+// USING SWITCH
+
+// switch, case, break, default
+
+// var marks = 80;
+// switch (true) {
+//     case marks >= 80 && marks <= 100:
+//         console.log('You Got A+');
+//         break;
+//     case marks >= 70 && marks <= 79:
+//         console.log('You Got A');
+//         break;
+//     case marks >= 60 && marks <= 69:
+//         console.log('You Got B+');
+//         break;
+//     case marks >= 50 && marks <= 59:
+//         console.log('You Got B');
+//         break;
+//     case marks >= 40 && marks <= 49:
+//         console.log('You Got C');
+//         break;
+//     case marks >= 33 && marks <= 39:
+//         console.log('You Got D');
+//         break;
+//     case marks >= 0 && marks <= 32:
+//         console.log('You Got F');
+//         break;
+//     default:
+//         console.log('Invalid Input');
+// }
+
+
+
+// =========================//=========================//=========================
+// প্রাকটিস চ্যালেঞ্জ-2: 
+// =========================//=========================//========================= 
+
+
+// USING IF/ELSE
+// var signal = 'green';
+// if (signal == 'red') {
+//     console.log("Don't Cross The Road Till Light Is Green.");
+// }
+// else if (signal == 'yellow') {
+//     console.log("Wait a bit, Green Light Will Turned On within short time.");
+// }
+// else if (signal == 'green') {
+//     console.log("Cross The Road Now.");
+// }
+// else {
+//     console.log("Light Nosto");
+// }
+
+
+// USING SWITCH
+// var switchSignal = 'red';
+// switch (true) {
+//     case switchSignal == 'red':
+//         console.log("Don't Cross The Road Till Light Is Green.");
+//         break;
+//     case switchSignal == 'yellow':
+//         console.log("Wait a bit, Green Light Will Turned On within short time.");
+//         break;
+//     case switchSignal == 'green':
+//         console.log("Cross The Road Now.");
+//         break;
+//     default:
+//         console.log("Light Nosto");
+// }
+
+
+
+// Drive TODO: https://drive.google.com/file/d/1bfxye7A1p-BBJCQCXaiUetl_qyS2Vc1E/view
+// =========================//=========================//=========================
+// Practice Problem 1:
+// =========================//=========================//=========================
+
+
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// console.log(fruits.indexOf('Banana'));
+// fruits[1] = 'Mango';
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.push('Watermelon');
+// console.log(fruits);
+// fruits.unshift('First');
+// console.log(fruits);
+// fruits.shift();
+// console.log(fruits);
+
+
+
+// =========================//=========================//=========================
+// Practice Problem 2:
+// =========================//=========================//=========================
+
+
+// let marks = 100;
+// if (marks >= 80 && marks <= 100) {
+//     console.log('You Got A+');
+// }
+// else if (marks >= 70 && marks <= 79) {
+//     console.log('You Got A');
+// }
+// else if (marks >= 60 && marks <= 69) {
+//     console.log('You Got B+');
+// }
+// else if (marks >= 50 && marks <= 59) {
+//     console.log('You Got B');
+// }
+// else if (marks >= 40 && marks <= 49) {
+//     console.log('You Got C');
+// }
+// else if (marks >= 33 && marks <= 39) {
+//     console.log('You Got D');
+// }
+// else if (marks >= 0 && marks <= 32) {
+//     console.log('You Failed');
+// }
+// else {
+//     console.log('Please Input a Valid Number from 0 to 100');
+// }
+
+
+// =========================//=========================//=========================
+// Practice Problem 3:
+// =========================//=========================//=========================
+// 1. You are given three numbers 13, 79, and 45. Write a program that will print the
+// largest number using if-else.
+
+// var num1 = 13;
+// var num2 = 09;
+// var num3 = 12;
+
+// if (num1 > num2 && num1 > num3) {
+//     console.log(num1, 'is a big one');
+// }
+// else if (num2 > num3 && num2 > num1) {
+//     console.log(num2, 'is a big one');
+// }
+// else {
+//     console.log(num3, 'is a big one');
+// }
+
+
+// 2. You are given a triangle with the sides 9, 8, 9. Write a program to check whether a
+// triangle is Isosceles or not using if-else.
+// Isosceles => two sides are equal
+
+// var x = 2;
+// var y = 2;
+// var z = 1;
+
+// if (x == y && y == z) {
+//     console.log("Somantoral Trivuj");
+// }
+// else if (x == y || y == z || z == x) {
+//     console.log("Somo Dibahu Trivuj");
+// }
+// else {
+//     console.log("Osomo Bahu Trivuh");
+// }
+
+
